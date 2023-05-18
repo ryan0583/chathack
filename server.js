4189ff5c6aad585294ee2e61cdedf467236fd846
ryan0583 <ryan0583@gmail.com>
2023-05-18 17:17:48 +0100

    Proper schemas

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const apiKey = require("./apiKey");
+const {eventSchema, playerSchema, eventTypeSchema, competitionSchema, seasonSchema, teamSchema, countrySchema, matchSchema} = require("./schema");
 
 const {Configuration, OpenAIApi} = require("openai");
 
 const config = new Configuration({
-    apiKey: "YOUR API KEY HERE"
+    apiKey: apiKey
 })
 
 const openai = new OpenAIApi(config);
@@ -18,18 +20,20 @@ app.use(cors());
 app.post("/chat", async(req, res) => {
     const {prompt} = req.body;
 
-    // const completion = await openai.createCompletion({
-    //     model: "text-davinci-003",
-    //     max_tokens: 512,
-    //     temperature: 0,
-    //     prompt: prompt
-    // });
-
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `### Postgres SQL tables, with their properties:\n#\n# Event(id, event_type, player_id)\n#\n### ${prompt}`,
+        prompt: `### postgres table schema:
+        ${eventSchema}\n#
+        ${eventTypeSchema}\n#
+        ${competitionSchema}\n#
+        ${countrySchema}\n#
+        ${seasonSchema}\n#
+        ${playerSchema}\n#
+        ${teamSchema}\n#
+        ${matchSchema}\n#
+        \n### ${prompt}. A shot is defined as an event_type of 'goal', 'miss', 'attempt_saved', 'post','attempt_blocked','attempt_saved_to_post','attempt_saved_off_target'.`,
         temperature: 0,
-        max_tokens: 150,
+        max_tokens: 1000,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import { useState } from 'react';
 function App() {
   const [prompt, setPrompt] = useState('');
   const [response, setResponse] = useState('');
+  const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   const HTTP = 'http://localhost:8020/chat';
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setError('');
     setResponse('');
     setLoading(true);
 
     axios
       .post(`${HTTP}`, { prompt })
       .then((res) => setResponse(res.data))
-      .catch((error) => console.log(error))
+      .catch((error) => setError(error))
       .finally(() => setLoading(false));
   };
 
@@ -27,6 +29,7 @@ function App() {
       className="App"
       style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}
     >
+      <h1>Event Query Generator</h1>
       <textarea
         style={{ width: '100%', height: '100px' }}
         type="text"
@@ -38,6 +41,7 @@ function App() {
       </div>
       {loading && 'Loading...'}
       {response}
+      {error}
     </div>
   );
 }
