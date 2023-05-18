c2e4ee255b5c82e10557b36a52888328abb5b14f
ryan0583 <ryan0583@gmail.com>
2023-05-18 18:31:09 +0100

    Running SQL against DB

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const apiKey = require("./apiKey");
-const {eventSchema, playerSchema, eventTypeSchema, competitionSchema, seasonSchema, teamSchema, countrySchema, matchSchema} = require("./schema");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const apiKey = require('./apiKey');
+const {
+  eventSchema,
+  playerSchema,
+  eventTypeSchema,
+  competitionSchema,
+  seasonSchema,
+  teamSchema,
+  countrySchema,
+  matchSchema,
+} = require('./schema');
+const { Client } = require('pg');
+const dbCreds = require('./dbCreds');
 
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const config = new Configuration({
-    apiKey: apiKey
-})
+  apiKey: apiKey,
+});
 
 const openai = new OpenAIApi(config);
 
@@ -17,12 +28,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/chat", async(req, res) => {
-    const {prompt} = req.body;
+app.post('/chat', async (req, res) => {
+  const { prompt } = req.body;
 
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `### postgres table schema:
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `### postgres table schema:
         ${eventSchema}\n#
         ${eventTypeSchema}\n#
         ${competitionSchema}\n#
@@ -31,18 +42,36 @@ app.post("/chat", async(req, res) => {
         ${playerSchema}\n#
         ${teamSchema}\n#
         ${matchSchema}\n#
-        \n### ${prompt}. A shot is defined as an event_type of 'goal', 'miss', 'attempt_saved', 'post','attempt_blocked','attempt_saved_to_post','attempt_saved_off_target'.`,
-        temperature: 0,
-        max_tokens: 1000,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["#", ";"],
-      });
-    res.send(completion.data.choices[0].text);
+        \n### ${prompt}`,
+    temperature: 0,
+    max_tokens: 1000,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ['#', ';'],
+  });
+  res.send(completion.data.choices[0].text);
+});
+
+const client = new Client({
+  user: 'iquser',
+  host: 'iq-testing-cluster.cluster-cv04zb8jr41a.eu-west-2.rds.amazonaws.com',
+  database: 'iqdb',
+  password: dbCreds,
+  port: 5432,
+});
+
+client.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+app.post('/sql', async (req, res) => {
+  const { sql } = req.body;
+  const sqlRes = await client.query(sql);
+  return res.send(sqlRes.rows);
 });
 
 const PORT = 8020;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
--- src/App.js
+++ src/App.js
@@ -7,19 +7,41 @@ function App() {
   const [response, setResponse] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const HTTP = 'http://localhost:8020/chat';
+  const [sqlLoading, setSqlLoading] = useState(false);
+  const [sqlResult, setSqlResult] = useState(null);
 
-  const handleSubmit = (e) => {
+  const handleError = (e) => {
+    console.log(error);
+    setError('Something went wrong :(');
+  };
+
+  const getQuery = (e) => {
     e.preventDefault();
     setError('');
     setResponse('');
+    setSqlLoading(false);
+    setSqlResult(null);
     setLoading(true);
 
     axios
-      .post(`${HTTP}`, { prompt })
+      .post('http://localhost:8020/chat', { prompt })
       .then((res) => setResponse(res.data))
-      .catch((error) => setError(error))
+      .catch(handleError)
       .finally(() => setLoading(false));
+  };
+
+  const runSql = (e) => {
+    e.preventDefault();
+    setSqlLoading(true);
+    setSqlResult(null);
+
+    axios
+      .post('http://localhost:8020/sql', {
+        sql: response,
+      })
+      .then((res) => setSqlResult(res.data))
+      .catch(handleError)
+      .finally(() => setSqlLoading(false));
   };
 
   const handlePrompt = (e) => setPrompt(e.target.value);
@@ -27,7 +49,12 @@ function App() {
   return (
     <div
       className="App"
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '10px',
+      }}
     >
       <h1>Event Query Generator</h1>
       <textarea
@@ -37,11 +64,35 @@ function App() {
         onChange={handlePrompt}
       />
       <div style={{ width: '100%', display: 'flex', alignItems: 'flex-start' }}>
-        <button onClick={handleSubmit}>GO!</button>
+        <button onClick={getQuery}>GO!</button>
       </div>
       {loading && 'Loading...'}
       {response}
       {error}
+      <div style={{ width: '100%', display: 'flex', alignItems: 'flex-start' }}>
+        <button onClick={runSql}>Run SQL</button>
+      </div>
+      {sqlLoading && 'Loading...'}
+      {sqlResult && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(sqlResult[0]).map((val, i) => (
+                <td key={i}>{val}</td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {sqlResult.map((result, i) => (
+              <tr key={i}>
+                {Object.values(result).map((val, i) => (
+                  <td key={i}>{val}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
